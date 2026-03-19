4fd13ddb5bb0f720ba510c56941b886d4435b671
Takiguchii <alexandre.pupim360@gmail.com>
2026-03-19 15:37:14 -0300

    refactor: refatorando dbcontext para receber novas entidades

--- tool-system/tool-system/Data/AppDbContext.cs
+++ tool-system/tool-system/Data/AppDbContext.cs
@@ -1,10 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using ToolingSystem.API.Models;
 
 namespace tool_system.Data;
-using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
     public DbSet<Empresa> Empresas { get; set; }
+    
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Macho> Machos { get; set; }
+    public DbSet<Molde> Moldes { get; set; }
 }
