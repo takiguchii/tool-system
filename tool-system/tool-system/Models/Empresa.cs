93577826b889dee882d7f933008482e3b7c4e2ca
Takiguchii <alexandre.pupim360@gmail.com>
2026-03-19 09:40:52 -0300

    feat: criando primeira model empresa.cs

---
+++ tool-system/tool-system/Models/Empresa.cs
@@ -0,0 +1,8 @@
+namespace tool_system.Models;
+
+public class Empresa
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string? Cidade { get; set; }
+}
